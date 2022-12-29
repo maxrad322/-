c9b1bfbcea5d871d5ee2a0af4e13618ea70925ec
maxrad322 <bloody4ch@mail.ru>
2022-12-29 18:06:48 +0300

    dz1-task5

--- дз1/задача 5/Source.cpp
+++ дз1/задача 5/Source.cpp
@@ -1,34 +1,21 @@
 #include <iostream>
 #include <locale>
-#include <cmath>
+
 using namespace std;
-int main()
-{
+
+int main() {
 	setlocale(LC_ALL, "rus");
-	// "Еще уравнение"
-	double a, b, c, D;
-	cout << "Введите a, b и c для уравнения вида ax^2 + bx + c = 0 \n";
-	cin >> a >> b >> c;
-	if (a == 0)
-	{
-		if (b != 0)
-			cout << "x = " << (-c) / b << endl;
-		else if (c == 0)
-			cout << "x - любое число" << endl;
-		else
-			cout << "Корней нет" << endl;
+	bool day, curtains, lamp;
+	cout << "Сейчас день? (1,0). Шторы раздвинуты? (1,0) Лампа горит? (1,0)";
+	cin >> day >> curtains >> lamp;
+	if (lamp) {
+		cout << "Lightly";
 	}
-	else
-	{
-		D = b * b - 4 * a*c;
-		if (D == 0)
-			cout << "x = " << (-b) / (2 * a) << endl;
-		if (D < 0)
-			cout << "Корней нет" << endl;
-		if (D > 0)
-		{
-			cout << "x1 = " << ((-b) + sqrt(D)) / (2 * a) << endl;
-			cout << "x2 = " << ((-b) - sqrt(D)) / (2 * a) << endl;
-		}
+	else if (day & curtains) {
+		cout << "Lightly";
 	}
+	else {
+		cout << "Dark";
+	}
+	return 0;
 }
